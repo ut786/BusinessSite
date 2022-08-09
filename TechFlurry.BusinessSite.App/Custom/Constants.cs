80565d4501e2284613e783003e794d8bc205733a
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2022-08-09 21:41:09 +0500

    added site changes

--- TechFlurry.BusinessSite.App/Custom/Constants.cs
+++ TechFlurry.BusinessSite.App/Custom/Constants.cs
@@ -3,11 +3,11 @@
     public static class Constants
     {
         public const string FACEBOOK_ADDRESS = "https://www.facebook.com/techflurry";
-        public const string LINKEDIN_ADDRESS = "https://www.linkedin.com/techflurry";
+        public const string LINKEDIN_ADDRESS = "https://www.linkedin.com/company/71980305/";
         public const string GMAIL_ADDRESS = "mailto:techflurry5@gmail.com";
         public const string INFO_EMAIL_ADDRESS = "info@techflurry.co";
         public const string PHONE_NUMBER = "(+92) 307 4179629";
         public const string GITHUB_ADDRESS = "https://www.github.com/techflurry";
-        public const string POSTAL_ADDRESS = "98, Dilkusha Park Rajgarh, Lahore, Pakistan";
+        public const string POSTAL_ADDRESS = "235-A, D Block, Edenabad, Lahore, Pakistan";
     }
 }
