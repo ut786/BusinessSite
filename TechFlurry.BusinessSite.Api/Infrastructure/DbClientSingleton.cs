f52c2a1f51d22f100813381026dbb7d853c2eb0c
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2022-08-10 17:48:41 +0500

    added cosmos db functionality

---
+++ TechFlurry.BusinessSite.Api/Infrastructure/DbClientSingleton.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Threading.Tasks;
+
+namespace TechFlurry.BusinessSite.Api.Infrastructure
+{
+    internal class DbClientSingleton
+    {
+        private CosmosClient cosmosClient;
+        private DatabaseResponse db;
+        private Container messsagesContainer;
+
+        private DbClientSingleton()
+        {
+            cosmosClient = new CosmosClient("AccountEndpoint=https://businesssite-db.documents.azure.com:443/;AccountKey=rEWEpFmLBSLxRmHaIDWYGbUHGxqCHyBqYzb5FHq2Y3zS8xWdxN61LnojiwO3YHET8322WeWz1ifjQzYDeHrUTQ==;");
+
+        }
+
+        private async Task InitClient()
+        {
+            db = await cosmosClient.CreateDatabaseIfNotExistsAsync("businesssite_db");
+        }
+
+
+        public Container GetMessagesContainer()
+        {
+            messsagesContainer ??= cosmosClient.GetContainer(db.Database.Id, "Messages");
+            return messsagesContainer;
+        }
+
+
+        public static Lazy<Task<DbClientSingleton>> Instance => new(async () =>
+            {
+                var dbClient = new DbClientSingleton();
+                await dbClient.InitClient();
+                return dbClient;
+            });
+    }
+}
--- Techflurry.BusinessSite.Models/ContactMessageModel.cs
+++ Techflurry.BusinessSite.Models/ContactMessageModel.cs
@@ -1,13 +1,16 @@
-﻿namespace TechFlurry.BusinessSite.Models
+﻿using Newtonsoft.Json;
+
+namespace TechFlurry.BusinessSite.Models
 {
     public class ContactMessageModel
     {
+        [JsonProperty(PropertyName = "id")]
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Subject { get; set; }
-        public string Services { get; set; }
+        public string Service { get; set; }
         public string Message { get; set; }
     }
 }
