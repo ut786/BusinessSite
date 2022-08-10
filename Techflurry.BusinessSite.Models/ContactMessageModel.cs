d3ffad2f646c9721d07ebbb18f162f0dc0a14306
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2022-08-10 12:53:30 +0500

    added function to test the workflow

---
+++ Techflurry.BusinessSite.Models/ContactMessageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TechFlurry.BusinessSite.Models
+{
+    public class ContactMessageModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string Services { get; set; }
+        public string Message { get; set; }
+    }
+}
