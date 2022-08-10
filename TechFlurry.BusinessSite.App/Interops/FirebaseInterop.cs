f06be1bb56321d779fc541a82c6c13506abb6c57
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2022-08-10 10:44:59 +0500

    added firebase configurations

---
+++ TechFlurry.BusinessSite.App/Interops/FirebaseInterop.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace TechFlurry.BusinessSite.App.Interops
+{
+    public interface IFirebaseInterop
+    {
+        Task InitFirebase();
+    }
+
+    public class FirebaseInterop : InteropBase, IFirebaseInterop
+    {
+        public FirebaseInterop(IJSRuntime jsRuntime) : base("./js/firebase.js", jsRuntime)
+        {
+        }
+
+
+        public async Task InitFirebase()
+        {
+            var module = await Module;
+            await module.InvokeVoidAsync("initFirebase");
+        }
+    }
+}
--- TechFlurry.BusinessSite.App/Program.cs
+++ TechFlurry.BusinessSite.App/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TechFlurry.BusinessSite.App;
+using TechFlurry.BusinessSite.App.Interops;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#wrapper");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IFirebaseInterop, FirebaseInterop>();
 
 await builder.Build().RunAsync();
